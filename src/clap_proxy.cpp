642ecad70cfc3c3edd406a940539308cd114f260
John Matter <jmatter4@gmail.com>
2026-05-27 01:20:26 -0400

    Query the clap.surround extension
    (cherry picked from commit 0924cbad394bb7cf7be3dfd9d96dddd2a6dfd704)

--- src/clap_proxy.cpp
+++ src/clap_proxy.cpp
@@ -229,6 +229,8 @@ void Plugin::connectClap(const clap_plugin_t *clap)
   if (!_ext._configurable_audio_ports)
     getExtension(_plugin, _ext._configurable_audio_ports, CLAP_EXT_CONFIGURABLE_AUDIO_PORTS_COMPAT);
   getExtension(_plugin, _ext._audio_ports_activation, CLAP_EXT_AUDIO_PORTS_ACTIVATION);
+  getExtension(_plugin, _ext._surround, CLAP_EXT_SURROUND);
+  if (!_ext._surround) getExtension(_plugin, _ext._surround, CLAP_EXT_SURROUND_COMPAT);
   getExtension(_plugin, _ext._noteports, CLAP_EXT_NOTE_PORTS);
   getExtension(_plugin, _ext._latency, CLAP_EXT_LATENCY);
   getExtension(_plugin, _ext._render, CLAP_EXT_RENDER);
